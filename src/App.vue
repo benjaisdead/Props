a2740b08bc583ec0e1f6d3a14133e77cbcb0e766
benjaisdead <40874757+benjaisdead@users.noreply.github.com>
2022-06-27 23:59:21 -0400

    Desafio

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{ mensaje }}
+    <button v-on:click="mensaje = 'hello world!!!'"> hola </button>
+    <button v-on:click="destroy()"> Destruir </button>
+    <HelloWorld msg="Mensaje del Padre"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      mensaje: 'hola mundo !!!',
+    }//Fin return
+  }, //fin data
+  beforeCreate(){
+      console.log('beforeCreate: ', this.mensaje);
+  },
+  created(){
+      console.log('created: ', this.mensaje);
+  },
+  beforeMount(){
+      console.log('beforeMount: ', this.mensaje);
+  },
+  mounted(){
+      console.log('mounted: ', this.mensaje);
+  },
+  beforeUpdate(){
+     console.log('beforeUpdate: ', this.mensaje);
+  },
+  updated(){
+    console.log('updated: ', this.mensaje);
+  },
+  beforeDestroy(){
+    console.log('beforeDestroy: ', this.mensaje);
+  },
+  destroyed(){
+    console.log('destroyed: ', this.mensaje);
+  },
+  methods:{
+    destroy(){
+        this.$destroy();
+    }
+  }
+}//Fin export
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <h2> {{ saludo }} </h2>
+    <p> Mensaje original: {{ mensaje }}</p>
+    <p> Mensaje al revés: {{ mensajeAlReves }}</p>
+    <p> Mensaje al revés con Método sin trampa: {{ mensajeAlRevesConMetodoSinTrampa() }} </p>
+    <p> Mensaje al revés con Método: <span id="unMensaje"></span></p> <button v-on:click="mensajeAlRevesConMetodo"> llamar metodo </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+        mensaje: 'hola mundo otra vez',
+        saludo: 'Bienvenidos',
+    }//Fin return
+  }, //fin data
+  computed:{
+    mensajeAlReves(){
+      return this.mensaje.split('').reverse().join('')
+    }//Fin mensajeAlReves
+  }, //Fin Computed
+  methods:{
+       mensajeAlRevesConMetodoSinTrampa(){
+          return this.mensaje.split('').reverse().join('');
+       },
+      mensajeAlRevesConMetodo(){
+            let elMensajeAlReves = this.mensaje.split('').reverse().join('');
+            let elSpan = document.getElementById('unMensaje');
+            elSpan.innerHTML = elMensajeAlReves;
+      }
+  }, //fin methods
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
